11b0bed932e578629bffa6e4ca7c1f1588ddb1e2
마광휘 <vallista@tistory.com>
2018-01-14 16:12:42 +0900

    [update] 알럿메시지 완성!

--- 2-AlertMessage/script.js
+++ 2-AlertMessage/script.js
@@ -3,7 +3,7 @@ Vue.component('button-component', {
     template: '<button @click="this.clickEvent">Alert Print!</button>',
     methods: {
         clickEvent: function() {
-            EventBus.$emit('addAlert', {title: 'test', description: 'test'});
+            EventBus.$emit('addAlert', { description: Math.random().toString()});
         }
     }
 });
@@ -11,36 +11,44 @@ Vue.component('button-component', {
 Vue.component('alert-box', {
     name: 'alert-box',
     props: {
-        title: {
-            type: String
-        },
-        description: {
-            type: String
-        }
+        index: '',
+        description: ''
     },
-    template: '<div class=""></div>'
+    mounted: function() {
+        setTimeout(function() {
+            EventBus.$emit('deleteAlert');
+        }, 2000);
+    },
+    template: '<div :class="`alert-box-${this.index}`" class="alert-box alert-box-start flex-container flex-center-sort flex-column">' +
+        '<div class="title flex-container flex-center-sort flex-column"><div class="title-image-background"></div><img class="title-image" src="image.png"/></div>' +
+        '<div class="description flex-container flex-center-sort flex-column">{{this.description}}</div>' +
+    '</div>'
 });
 
 Vue.component('alert-box-list', {
     name: 'alert-box-list',
     data: function() {
         return {
-            alertList: {
-                type: Array
-            }
+            alertList: []
         };
     },
     methods: {
-        addAlert: function({ title, description }) {
-            this.alertList.push({ title, description });
-            console.log(this.alertList);
+        addAlert: function({ description }) {
+            this.alertList.push({ description });
+        },
+        deleteAlert: function() {
+            this.alertList.shift();
         }
     },
     created: function() {
         this.alertList = [];
         EventBus.$on('addAlert', this.addAlert);
+        EventBus.$on('deleteAlert', this.deleteAlert);
     },
-    template: '<ul><li v-for="alert in alertList"><alert-box :title="alert.title" :description="alert.description"></alert-box></li></ul>'
+    template: '<ul><li v-for="(value, index) in alertList">' +
+    '<alert-box :description="value.description" :index="index" :key="index">' +
+    '</alert-box>' +
+    '</li></ul>'
 });
 
 var EventBus = new Vue();
